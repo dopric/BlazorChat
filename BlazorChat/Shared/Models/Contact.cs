94c10fa8e24053e2f4ba1b7f91dcd1384cc73a46
Dragan Opric <dragan.opric@team-w.ch>
2023-08-19 09:00:51 +0200

    Projektdateien hinzufügen.

---
+++ BlazorChat/Shared/Models/Contact.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorChat.Shared.Models
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        public Contact()
+        {
+
+        }
+
+        public Contact(int id, string firstName, string lastName)
+        {
+            ContactId = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
